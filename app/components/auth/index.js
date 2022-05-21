bb223a973b12b14f0943dd51550f6c222bd46965
byein <kyebin0825@gmail.com>
2022-05-21 19:03:57 +0900

    feat: initial project setting & redux setting & navigation setting

---
+++ app/components/auth/index.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const AuthComponent = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Auth Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default AuthComponent;
